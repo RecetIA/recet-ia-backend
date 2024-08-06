579e27dd9bc8a13c446a7d67526730db2f831289
JKmoises <moi.prado@duocuc.cl>
2024-08-06 10:44:30 -0400

    Actualizar a 7d duracion de JWT

--- src/config/adapters/jwt.adapter.ts
+++ src/config/adapters/jwt.adapter.ts
@@ -5,7 +5,7 @@ const JWT_SEED = envs.JWT_SEED;
 export class JwtAdapter {
   static async generateToken(
     payload: any,
-    duration: string = '1d',
+    duration: string = '7d',
   ): Promise<string> {
     return new Promise((resolve, reject) => {
       sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
